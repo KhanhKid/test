d405c53576e807c6857bddf96bcbe3457e783a6e
KhanhKid <sachlong112359@gmail.com>
2018-03-23 11:37:58 +0700

    update change color

--- fuel/app/modules/frontend/views/index/index.php
+++ fuel/app/modules/frontend/views/index/index.php
@@ -4,22 +4,22 @@
             <div class="col-lg-8 content order-lg-1 order-2">
                 <h2 class="title" style="text-align: center;
     color: #eb3b5a;
-    text-shadow: -1px 0 #fc5c65, 0 1px #fc5c65, 1px 0 black, 0 -1px black;">CÔNG TY TRÁCH NHIỆM HỮU HẠN THƯƠNG MẠI - XỬ LÝ MÔI TRƯỜNG THÀNH LẬP</h2>
+    text-shadow: -2px 0 #fc5c65, 0 2px #fc5c65, 1px 0 black, 0 -1px black;">CÔNG TY TRÁCH NHIỆM HỮU HẠN THƯƠNG MẠI - XỬ LÝ MÔI TRƯỜNG THÀNH LẬP</h2>
                 <p>- Với kinh nghiệm hơn 15 năm trong lĩnh vực xử lý và tái chế chất thải, là một trong những đơn vị đi đầu trong lĩnh vực trên đang ngày càng phát triển, đầu tư thêm trang thiết bị mới, hiện đại,… được Quý Khách hàng đánh giá cao về chất lượng sản phẩm dịch vụ sẽ là sự lựa chọn tốt nhất cho các Khách hàng trong việc tìm kiếm các đơn vị có đầy đủ năng lực và tiết kiệm chi phí cho việc xử lý chất thải.</br>- Xin chân thành cảm ơn Quý Khách hàng đã tín nhiệm và hợp tác với chúng tôi trong thời gian qua. Công ty chúng tôi cam kết sẽ đồng hành cùng Quý khách trong công tác bảo vệ môi trường và phát triển bền vững với dịch vụ tốt nhất, giá cả cạnh tranh và hợp lý nhất. Hy vọng, Công ty chúng tôi sẽ đóng góp một phần nhỏ vào sự thành công của Quý  khách.
                 </p>
                 <div class="icon-box wow fadeInUp">
                     <div class="icon"><i class="fa fa-shopping-bag"></i></div>
-                    <h4 class="title"><a href="">XỬ LÝ CHẤT THẢI</a></h4>
+                    <h3 class="title"><b>XỬ LÝ CHẤT THẢI</b></h3>
                     <p class="description">Xử lý chất thải bao gồm: chất thải nguy hại và chất thải không nguy hại là khâu then chốt trong phát triển sản xuất sạch hơn và tiêu dùng sạch hơn, hướng tới nền công nghiệp xanh.</p>
                 </div>
                 <div class="icon-box wow fadeInUp" data-wow-delay="0.2s">
                     <div class="icon"><i class="fa fa-photo"></i></div>
-                    <h4 class="title"><a href="">THU MUA PHẾ LIỆU</a></h4>
+                    <h3 class="title"><b>THU MUA PHẾ LIỆU</b></h3>
                     <p class="description">Thu mua phế liệu từ công trình, nhà xưởng, hàng hóa thanh lý, các loại phế liệu đồng, nhôm, chì, kẽm, sắt, phế liệu nhựa, giấy, dầu nhớt, dung môi,…</p>
                 </div>
                 <div class="icon-box wow fadeInUp" data-wow-delay="0.4s">
                     <div class="icon"><i class="fa fa-bar-chart"></i></div>
-                    <h4 class="title"><a href="">TƯ VẤN MÔI TRƯỜNG</a></h4>
+                    <h3 class="title"><b>TƯ VẤN MÔI TRƯỜNG</b></h3>
                     <p class="description">Tư vấn giải pháp xử lý môi trường phù hợp, đảm bảo hiệu quả xử lý, tiết kiệm chi phí đầu tư, tái chế chất thải.</p>
                 </div>
             </div>
