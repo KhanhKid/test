af23a85f36ae25e32f0bf6b5ee4214f3de849cd5
KhanhKid <sachlong112359@gmail.com>
2018-04-14 14:34:48 +0700

    add orange color

--- fuel/app/modules/frontend/views/introduction/thietbicongnghe.php
+++ fuel/app/modules/frontend/views/introduction/thietbicongnghe.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
     <div class="col-lg-12 col-md-10 mx-auto">
-        <h1 class="color_green" style="text-align: center;">THIẾT BỊ - CÔNG NGHỆ</h1>
+        <h1 class="color_orange" style="text-align: center;">THIẾT BỊ - CÔNG NGHỆ</h1>
         <hr>
         <ul>
             <li><label class="btn_up"> Hệ thống lò đốt 
